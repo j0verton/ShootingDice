f7cbee7c35747572de79bf468e5db72a329ba6b4
Joe <overtonje@gmail.com>
2020-11-30 14:21:41 -0600

    got a version of smacktalkingplayer done, needs a refine though i think

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,11 @@
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
+    public class CreativeSmackTalkingPlayer : SmackTalkingPlayer
     {
+
+
+
 
     }
 }
